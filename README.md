c387922f373b3d1f68c000fbf8f73795b6caaf0e
Juan Ricardo <juan.ricardo.ga.me@gmail.com>
2025-05-09 14:26:25 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# consultorio
+Assistant oriented scheduling application
---
+++ consultorio/src/test/java/com/kosmos_test/asistente/consultorio/ConsultorioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosmos_test.asistente.consultorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
